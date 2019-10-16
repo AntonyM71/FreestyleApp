8840326a6f973d005883e305c30b1b1dceef9d13
Antony Mitchell <antony.mitchell@tessella.com>
2019-10-16 20:47:55 +0100

    stylesheet

--- styles.js
+++ styles.js
@@ -5,87 +5,40 @@ export const styles = StyleSheet.create({
       flex: 1,
       backgroundColor: '#fff',
     },
-    developmentModeText: {
-      marginBottom: 20,
-      color: 'rgba(0,0,0,0.4)',
-      fontSize: 14,
-      lineHeight: 19,
-      textAlign: 'center',
-    },
     contentContainer: {
-      paddingTop: 30,
+      paddingTop: 10,
     },
-    welcomeContainer: {
-      alignItems: 'center',
-      marginTop: 10,
-      marginBottom: 20,
+    noMove: {
+        backgroundColor:  "#4F84C4",
+        marginLeft: 4,
+        marginRight: 4, 
+        marginTop: 15,
+        
     },
-    welcomeImage: {
-      width: 100,
-      height: 80,
-      resizeMode: 'contain',
-      marginTop: 3,
-      marginLeft: -10,
+    noBonus: {
+        backgroundColor:  "#4F84C4",
+        marginLeft: 4,
+        marginRight: 4, 
+        marginTop: 8,
     },
-    getStartedContainer: {
-      alignItems: 'center',
-      marginHorizontal: 50,
+    moveScored: {
+        backgroundColor: "#223A5E",
+        marginLeft: 4,
+        marginRight: 4, 
+        marginTop: 15
     },
-    homeScreenFilename: {
-      marginVertical: 7,
-    },
-    codeHighlightText: {
-      color: 'rgba(96,100,109, 0.8)',
-    },
-    codeHighlightContainer: {
-      backgroundColor: 'rgba(0,0,0,0.05)',
-      borderRadius: 3,
-      paddingHorizontal: 4,
-    },
-    getStartedText: {
-      fontSize: 17,
-      color: 'rgba(96,100,109, 1)',
-      lineHeight: 24,
-      textAlign: 'center',
-    },
-    tabBarInfoContainer: {
-      position: 'absolute',
-      bottom: 0,
-      left: 0,
-      right: 0,
-      ...Platform.select({
-        ios: {
-          shadowColor: 'black',
-          shadowOffset: { height: -3 },
-          shadowOpacity: 0.1,
-          shadowRadius: 3,
-        },
-        android: {
-          elevation: 20,
-        },
-      }),
-      alignItems: 'center',
-      backgroundColor: '#fbfbfb',
-      paddingVertical: 20,
-    },
-    tabBarInfoText: {
-      fontSize: 17,
-      color: 'rgba(96,100,109, 1)',
-      textAlign: 'center',
-    },
-    navigationFilename: {
-      marginTop: 5,
-    },
-    ICFContainer: {
-      marginTop: 15,
-      alignItems: 'center',
-    },
-    ICFLink: {
-      paddingVertical: 15,
-    },
-    ICFLinkText: {
-      fontSize: 14,
-      color: '#2e78b7',
-    },
+    bonusScored: {
+        backgroundColor: "#223A5E",
+        marginLeft: 4,
+        marginRight: 4, 
+        marginTop:8,
+  },
+        deleteButton: {
+        backgroundColor: "#DC4C46",
+        marginLeft: 4,
+        marginRight: 4, 
+        marginTop:8,
+  },
+
   });
   
