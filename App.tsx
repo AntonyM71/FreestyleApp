3d94c0d0ca99cc59c2eb3732b6408ae7e1c17cbe
Ant-Mac <antonymitchell@hotmail.com>
2024-02-04 14:23:39 +0000

    Update splash Screen

--- App.tsx
+++ App.tsx
@@ -1,6 +1,6 @@
-import AppLoading from "expo-app-loading"
 import * as Font from "expo-font"
-import React from "react"
+import * as SplashScreen from "expo-splash-screen"
+import React, { useCallback, useEffect, useState } from "react"
 import { Platform, StatusBar, StyleSheet, View } from "react-native"
 import { Provider } from "react-redux"
 import AppNavigator from "./src/navigation/AppNavigator"
@@ -8,52 +8,57 @@ import configureStore from "./src/store"
 
 const store = configureStore()
 
-export default class App extends React.Component {
-	state = {
-		isLoadingComplete: false
-	}
+// Keep the splash screen visible while we fetch resources
+void SplashScreen.preventAutoHideAsync()
 
-	render() {
-		if (!this.state.isLoadingComplete) {
-			return (
-				<AppLoading
-					startAsync={this._loadResourcesAsync}
-					onError={this.handleLoadingError}
-					onFinish={this.handleFinishLoading}
-				/>
-			)
+export default function App() {
+	const [appIsReady, setAppIsReady] = useState(false)
+
+	useEffect(() => {
+		async function prepare() {
+			try {
+				// Pre-load fonts, make any API calls you need to do here
+				await Font.loadAsync({
+					// This is the font that we are using for our tab bar
+					// ...Icon.Ionicons.font,
+					// We include SpaceMono because we use it in HomeScreen.js. Feel free
+					// to remove this if you are not using it in your app
+					"space-mono": require("./src/assets/fonts/SpaceMono-Regular.ttf")
+				})
+			} catch (e) {
+				console.warn(e)
+			} finally {
+				// Tell the application to render
+				setAppIsReady(true)
+			}
 		}
 
-		return (
-			<Provider store={store}>
-				<View style={styles.container}>
-					{Platform.OS === "ios" && <StatusBar barStyle="default" />}
-					<AppNavigator />
-				</View>
-			</Provider>
-		)
+		void prepare()
+	}, [])
+
+	const onLayoutRootView = useCallback(async () => {
+		if (appIsReady) {
+			// This tells the splash screen to hide immediately! If we call this after
+			// `setAppIsReady`, then we may see a blank screen while the app is
+			// loading its initial state and rendering its first pixels. So instead,
+			// we hide the splash screen once we know the root view has already
+			// performed layout.
+			await SplashScreen.hideAsync()
+		}
+	}, [appIsReady])
+
+	if (!appIsReady) {
+		return null
 	}
 
-	_loadResourcesAsync = async () =>
-		new Promise(
-			Font.loadAsync({
-				// This is the font that we are using for our tab bar
-				// ...Icon.Ionicons.font,
-				// We include SpaceMono because we use it in HomeScreen.js. Feel free
-				// to remove this if you are not using it in your app
-				"space-mono": require("./src/assets/fonts/SpaceMono-Regular.ttf")
-			})
-		)
-
-	handleLoadingError = (error: any) => {
-		// In this case, you might want to report the error to your error
-		// reporting service, for example Sentry
-		console.warn(error)
-	}
-
-	handleFinishLoading = () => {
-		this.setState({ isLoadingComplete: true })
-	}
+	return (
+		<Provider store={store}>
+			<View style={styles.container} onLayout={onLayoutRootView}>
+				{Platform.OS === "ios" && <StatusBar barStyle="default" />}
+				<AppNavigator />
+			</View>
+		</Provider>
+	)
 }
 
 const styles = StyleSheet.create({
